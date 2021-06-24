a7bc742446aba3b07203d8f1763a6bf7cfc39a94
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-24 14:34:50 -0700

    adds background color

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express()
 
 const path = require('path')
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })

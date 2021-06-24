45ab4f5003d0bc2900d0a3115022e81942d792b2
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-24 15:14:21 -0700

    deploy test 7

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const app = express()
 const path = require('path')
 
 // app.use('/static', express.static(__dirname + '/public'))
-app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
